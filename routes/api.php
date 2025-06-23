e52bd5ad22faf23f1359d78a73ff2c3097cba452
Karla G <karla.gomez.23e@utzmg.edu.mx>
2025-06-23 10:18:02 -0600

    Tarea: Autenticacion

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MultaController;
+use App\Models\User;
 
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
@@ -10,3 +11,9 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('/multas', [MultaController::class, 'index']);
+
+  
+Route::middleware(['auth:sanctum', 'role:admin'])->group(function () {
+    Route::get('/multas', [MultaController::class, 'index']);
+    Route::post('/multas', [MultaController::class, 'store']);
+});
